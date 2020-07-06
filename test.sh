a7257bf8cd5c4b0703dc5b3fee81ea9bbb35931d
magicnana <magicnana999@126.com>
2020-07-06 14:32:00 +0800

    test

--- test.sh
+++ test.sh
@@ -1,11 +1,11 @@
 #!/bin/bash
 
-string=$INPUT_PAIRS
+string=$1
 array=(${string//,/ })
 
 for var in ${array[@]}
 do
    key=${var%:*}
    value=${var#*:}
-   sed -i "s#$key#$value#g" `grep $key -rl ./*.yaml`
+   sed -i "s#$key#$value#g" `grep '$key' example/application.yaml`
 done
